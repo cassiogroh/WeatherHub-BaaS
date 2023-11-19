f145161a51690a1a51770edaf708d38d784ee139
Cassio Groh <cassio_groh@hotmail.com>
2023-11-19 23:50:29 +0100

    Avoid using mock in production

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,12 +13,14 @@ const firestore = getFirestore(firebaseApp);
 const functions = getFunctions(firebaseApp);
 const auth = getAuth(firebaseApp);
 
-const USE_MOCK = false; // false to get requests from WU via API key/true for instant mock data
+const USE_MOCK = true; // false to get requests from WU via API key/true for instant mock data
+const IS_PRODUCTION = window.location.host === "weatherhub.app";
+
 const callableFunction = async (functionName: string, params?: any) => {
   const untypedMock = mocks as any;
   const mockData = untypedMock[functionName];
 
-  if (USE_MOCK && mockData) return mockData;
+  if (USE_MOCK && !IS_PRODUCTION && mockData) return mockData;
 
   const functionInstance = httpsCallable(functions, functionName);
   const response: any = await functionInstance(params);
